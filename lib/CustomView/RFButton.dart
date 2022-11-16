9e1d6f0591ea2bab56635371760c4adb5f753bc2
alumno <fouad>
2022-11-16 10:18:04 +0100

    Parte 2:       * la clase RFButton

---
+++ lib/CustomView/RFButton.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class RFButton extends StatelessWidget{
+  const RFButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+
+
+}
