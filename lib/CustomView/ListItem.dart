b78f1ee34d3aef398610e01b0d00c530323871c4
alumno <fouad>
2022-11-16 10:15:14 +0100

    Parte 2:       * la clase ListItem

---
+++ lib/CustomView/ListItem.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ListItem extends StatelessWidget{
+  const ListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+
+}
