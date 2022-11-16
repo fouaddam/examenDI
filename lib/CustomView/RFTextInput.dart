be3c00fa065df2db3aa6380f6426f033cd19ea9a
alumno <fouad>
2022-11-16 10:10:01 +0100

    Parte 2:     -he creado la carpeta CostumViews:       * la clase RFTextInput

---
+++ lib/CustomView/RFTextInput.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+
+class RFTextInput extends StatelessWidget{
+  const RFTextInput({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+
+
+}
