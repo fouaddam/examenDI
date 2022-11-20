e58136a64ec8425516757aae184d35d170121b33
Jason Albino <jason.albino@gruposolutio.com>
2022-11-20 19:25:20 +0100

    Parte 2:    -crear RFButton    -la carpeta assets

--- lib/CustomView/RFButton.dart
+++ lib/CustomView/RFButton.dart
@@ -1,14 +1,23 @@
 
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RFButton extends StatelessWidget{
-  const RFButton({super.key});
+  final String textButton;
+
+  const RFButton({super.key, required this.textButton});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return OutlinedButton.icon(
+      onPressed: () {
+        // Respond to button press
+      },
+      icon: Icon(Icons.add, size: 18),
+      label: Text(textButton),
+    );
   }
 
 
